140b56e24f8bd7a863785611930c908b7e950ede
Amir Lebba <Amirlebba@gmail.com>
2024-11-09 07:02:54 +0100

    Almost there

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import NavBar from "./NavBar/navBar";
 import About from "./About/About";
-import Skills from './Skills/Skills.jsx'
+import Skills from "./Skills/Skills.jsx";
 import Portfolio from "./Portfolio/Portfolio.jsx";
+import ContactMe from "./Contact/ContactMe.jsx";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
       <About />
       <Skills />
       <Portfolio />
+      <ContactMe />
     </div>
   );
 }
---
+++ src/Contact/ContactMe.jsx
@@ -0,0 +1,37 @@
+import "./Contact.css";
+import EnvelopeIcon from "./Asset/EnvlopeIcon";
+import MapPin from "./Asset/mapPin";
+import Phone from "./Asset/phone.jsx";
+function ContactMe() {
+  return (
+    <section className="contactMe-Wrapper">
+      <fieldset>
+        <legend>
+          <p>Contact Me</p>
+        </legend>
+        <div className="box">
+          <div className="content">
+            <div className="logos">
+              <EnvelopeIcon width="30px" height="30px" fill="white" />
+            </div>
+            <p id="text">Amirlebba@gmail.com</p>
+          </div>
+          <div className="content">
+            <div className="logos">
+              <MapPin width="30" height="30" fill="white" />
+            </div>
+            <p id="text">Algeria,El-Oued</p>
+          </div>
+          <div className="content">
+            <div className="logos">
+              <Phone width="30" height="30" fill="white" />
+            </div>
+            <p id="text">+213 672689842</p>
+          </div>
+        </div>
+      </fieldset>
+    </section>
+  );
+}
+
+export default ContactMe;
